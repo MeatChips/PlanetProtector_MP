4b96a1c9bb9019de30d8d470b7b5c8e3af19e3d2
MeatChips <84376178+MeatChips@users.noreply.github.com>
2024-03-05 09:21:23 +0100

    Projectile Fix + Removed URP Files
    Projectile gets destroyed after some time of not hitting something
    Removed unused URP files

--- PlanetProtector/Assets/Scripts/Projectile.cs
+++ PlanetProtector/Assets/Scripts/Projectile.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    private float destroyTimer = 50f;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        destroyTimer -= Time.deltaTime;
+        if(destroyTimer <= 0)
         {
             Destroy(this.gameObject);
         }
